a083142c492aa4d3698970b7ff6a1fbe3cd1cec8
joranvar <bart.post@gmail.com>
2017-04-01 14:46:20 +0200

    Write placeholder for 2008-Q-A

--- src/gcj-lib/Y2008/Q/A.hs
+++ src/gcj-lib/Y2008/Q/A.hs
@@ -2,5 +2,23 @@ module Y2008.Q.A where
 
 import Protolude
 
+import qualified Data.Text as Text (concat)
+import Data.String
+
 solve :: Text -> Text
-solve = identity
+solve =  Text.concat . map solve' . parse . drop 1 . lines . toS
+
+data P = P { ss :: [String]
+           , qs :: [String]
+           }
+
+parse :: [String] -> [P]
+solve' :: P -> Text
+
+parse [] = []
+parse (s:rest) =
+  let (ss', (q:rest')) = splitAt (maybe 0 fst $ head $ reads s) rest
+      (qs', rest'') = splitAt (maybe 0 fst $ head $ reads q) rest'
+  in P ss' qs' : parse rest''
+
+solve' (P _ _) = "Done"
