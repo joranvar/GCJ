9c16e3b0099e2d212af7a2673963526caa2aa2bf
joranvar <bart.post@gmail.com>
2017-04-01 14:55:14 +0200

    Add projectile commands

---
+++ .dir-locals.el
@@ -0,0 +1,3 @@
+((nil . ((projectile-project-test-cmd . "stack build --exec \"gcj 2008-Q-A\" < sample.txt")
+         (projectile-project-compilation-cmd . "stack build")
+         (projectile-project-run-cmd . "stack build --exec \"gcj 2008-Q-A\" < in.txt"))))
