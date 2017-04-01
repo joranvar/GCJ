dd9d135d7ee679678b18e22fa1c0be1d176a87d7
joranvar <bart.post@gmail.com>
2017-04-01 16:43:24 +0200

    Add a basic placeholder for C

--- src/gcj-lib/Lib.hs
+++ src/gcj-lib/Lib.hs
@@ -6,8 +6,10 @@ import qualified Data.Map as Map
 
 import qualified Y2008.Q.A (solve)
 import qualified Y2008.Q.B (solve)
+import qualified Y2008.Q.C (solve)
 
 runner :: Text -> Maybe (Text -> Text)
 runner = flip Map.lookup $ Map.fromList
   [ ("2008-Q-A", Y2008.Q.A.solve)
-  , ("2008-Q-B", Y2008.Q.B.solve) ]
+  , ("2008-Q-B", Y2008.Q.B.solve)
+  , ("2008-Q-C", Y2008.Q.C.solve) ]
---
+++ src/gcj-lib/Y2008/Q/C.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE RecordWildCards #-}
+
+module Y2008.Q.C where
+
+import Protolude
+
+import qualified Data.Text as Text (concat)
+import Data.String
+
+solve :: Text -> Text
+solve =  Text.concat . map write . zip [1..] .  map solve' . parse . drop 1 . lines . toS
+
+data P = P { f, bigR, t, r, g :: Double }
+
+data S = S Double
+
+parse :: [String] -> [P]
+solve' :: P -> S
+write :: (Int, S) -> Text
+
+read :: (Read a) => a -> String -> a
+read d = maybe d fst . head . reads
+
+parse (p:rest) =
+  let (f':bigR':t':r':g':_) = map (read 0.0) $ words p
+  in P f' bigR' t' r' g' : parse rest
+parse _ = []
+
+solve' P{..} | f >= (g/2) = S 1
+solve' P{..} = S 0
+
+write (i, S s) = toS $ "Case #" ++ show i ++ ": " ++ show s ++ "\n"
