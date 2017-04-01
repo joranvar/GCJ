d765708f66e2ffc932e36ec5517d2776f6ccb3dd
joranvar <bart.post@gmail.com>
2017-04-01 14:55:46 +0200

    Split writing from solving

--- src/gcj-lib/Y2008/Q/A.hs
+++ src/gcj-lib/Y2008/Q/A.hs
@@ -6,14 +6,17 @@ import qualified Data.Text as Text (concat)
 import Data.String
 
 solve :: Text -> Text
-solve =  Text.concat . map solve' . parse . drop 1 . lines . toS
+solve =  Text.concat . map write . zip [1..] .  map solve' . parse . drop 1 . lines . toS
 
 data P = P { ss :: [String]
            , qs :: [String]
            }
 
+data S = S Int
+
 parse :: [String] -> [P]
-solve' :: P -> Text
+solve' :: P -> S
+write :: (Int, S) -> Text
 
 parse [] = []
 parse (s:rest) =
@@ -21,4 +24,6 @@ parse (s:rest) =
       (qs', rest'') = splitAt (maybe 0 fst $ head $ reads q) rest'
   in P ss' qs' : parse rest''
 
-solve' (P _ _) = "Done"
+solve' (P _ _) = S 1
+
+write (i, S s) = toS $ "Case #" ++ show i ++ ": " ++ show s ++ "\n"
