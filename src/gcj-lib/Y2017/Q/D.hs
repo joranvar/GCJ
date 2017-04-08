626e3ce4f55ee7845b2cf5f5b8fdc92e52d6113c
joranvar <bart.post@gmail.com>
2017-04-08 15:59:31 +0200

    Parse Y2017.Q.D

--- src/gcj-lib/Y2017/Q/D.hs
+++ src/gcj-lib/Y2017/Q/D.hs
@@ -7,27 +7,44 @@ import qualified Data.Text as Text (concat)
 import Data.String
 
 solve :: Bool -> Text -> Text
-solve _ =  Text.concat . map write . zip [1..] .  map solve' . parse . drop 1 . lines . toS
+solve blindly =  Text.concat . map write . zip [1..] .  map (\p -> let s = solve' p in (s, if blindly then (True, "") else verify p s)) . parse . drop 1 . lines . toS
 
-data P = P
+type Model = Char
+data Placement = Placement { m::Model, r, c::Int }
+  deriving Show
+data P = P { n::Int, models::[Placement]  }
   deriving Show
 
-data S = S
+data S = S { y, z::Int, changedModels::[Placement] }
 
 parse :: [String] -> [P]
 solve' :: P -> S
-write :: (Int, S) -> Text
+write :: (Int, (S, (Bool, String))) -> Text
+verify :: P -> S -> (Bool, String)
 
 read :: (Read a) => a -> String -> a
 read d = maybe d fst . head . reads
 
+parse (nm:rest) =
+  let (n':m':_) = map (read 0) $ words nm
+      (ms', rest') = splitAt m' rest
+      parsePlacement pl = let ((m'':_):r':c':_) = words pl in Placement m'' (read 0 r') (read 0 c')
+  in P n' (map parsePlacement ms') : parse rest'
 parse _ = []
 
-{-> parse . drop 1 . lines $ "2\n5\n3\n1 1 1\n2 1 0 2 0\n1 5 0\n1\n2\n1 1 0\n1 1 1\n"
+{-> parse . drop 1 . lines $ "3\n2 0\n1 1\no 1 1\n3 4\n+ 2 3\n+ 2 1\nx 3 1\n+ 2 2"
 
-[P {n = 5, ms = [[(1,True)],[(0,False),(0,False)],[(0,False)]]},P {n = 1, ms = [[(0,False)],[(1,True)]]}]
+[P {n = 2, models = []},P {n = 1, models = [Placement {m = 'o', r = 1, c = 1}]},P {n = 3, models = [Placement {m = '+', r = 2, c = 3},Placement {m = '+', r = 2, c = 1},Placement {m = 'x', r = 3, c = 1},Placement {m = '+', r = 2, c = 2}]}]
 -}
 
-solve' P = S
+solve' P{..} = S 0 0 []
 
-write (i, S) = toS $ "Case #" ++ show i ++ ":" ++ "\n"
+write (i, (S y z ms, (True, _))) = toS $ "Case #" ++ show i ++ ": " ++ show y ++ " " ++ show z ++ "\n" ++ concatMap ((++ "\n") . writePlacement) ms
+write (i, (S y z ms, (False, err))) = toS $ "!(" ++ err ++ ")Case #" ++ show i ++ ": " ++ show y ++ " " ++ show z ++ "\n" ++ concatMap ((++ "\n") . writePlacement) ms
+
+writePlacement :: Placement -> String
+writePlacement Placement{..} = writeModel m ++ " " ++ show r ++ " " ++ show c
+writeModel :: Model -> String
+writeModel = (:[])
+
+verify P{..} S{..} = (False, "")
