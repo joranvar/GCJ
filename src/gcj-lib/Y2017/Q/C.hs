e5cd8e3e8249aa2f1ca34dd84ef34efac02eb57a
joranvar <bart.post@gmail.com>
2017-04-08 09:59:51 +0200

    Parse 2017.Q.C

--- src/gcj-lib/Y2017/Q/C.hs
+++ src/gcj-lib/Y2017/Q/C.hs
@@ -9,10 +9,10 @@ import Data.String
 solve :: Text -> Text
 solve =  Text.concat . map write . zip [1..] .  map solve' . parse . drop 1 . lines . toS
 
-data P = P
+data P = P { n::Integer, k::Integer}
   deriving Show
 
-data S = S
+data S = S Integer Integer
 
 parse :: [String] -> [P]
 solve' :: P -> S
@@ -21,13 +21,16 @@ write :: (Int, S) -> Text
 read :: (Read a) => a -> String -> a
 read d = maybe d fst . head . reads
 
+parse (nk:rest) =
+  let (n':k':_) = map (read 0) $ words nk
+  in P n' k' : parse rest
 parse _ = []
 
-{-> parse . drop 1 . lines $ "2\n5\n3\n1 1 1\n2 1 0 2 0\n1 5 0\n1\n2\n1 1 0\n1 1 1\n"
+{-> parse . drop 1 . lines $ "5\n4 2\n5 2\n6 2\n1000 1000\n1000 1\n"
 
-[P {n = 5, ms = [[(1,True)],[(0,False),(0,False)],[(0,False)]]},P {n = 1, ms = [[(0,False)],[(1,True)]]}]
+[P {n = 4, k = 2},P {n = 5, k = 2},P {n = 6, k = 2},P {n = 1000, k = 1000},P {n = 1000, k = 1}]
 -}
 
-solve' P = S
+solve' P{..} = S 0 0
 
-write (i, S) = toS $ "Case #" ++ show i ++ ":" ++ "\n"
+write (i, S minLR maxLR) = toS $ "Case #" ++ show i ++ ": " ++ show minLR ++ " " ++ show maxLR ++ "\n"
