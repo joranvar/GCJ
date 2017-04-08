96dee52fd09f53814b134985ec1f55b060c71984
joranvar <bart.post@gmail.com>
2017-04-08 08:45:35 +0200

    Add placeholders for 2017.Q

--- src/gcj-lib/Lib.hs
+++ src/gcj-lib/Lib.hs
@@ -9,6 +9,10 @@ import qualified Y2008.Q.B (solve)
 import qualified Y2008.Q.C (solve)
 import qualified Y2008.R1A.A (solve)
 import qualified Y2008.R1A.B (solve)
+import qualified Y2017.Q.A (solve)
+import qualified Y2017.Q.B (solve)
+import qualified Y2017.Q.C (solve)
+import qualified Y2017.Q.D (solve)
 
 runner :: Text -> Maybe (Text -> Text)
 runner = flip Map.lookup $ Map.fromList
@@ -16,4 +20,8 @@ runner = flip Map.lookup $ Map.fromList
   , ("2008-Q-B", Y2008.Q.B.solve)
   , ("2008-Q-C", Y2008.Q.C.solve)
   , ("2008-R1A-A", Y2008.R1A.A.solve)
-  , ("2008-R1A-B", Y2008.R1A.B.solve) ]
+  , ("2008-R1A-B", Y2008.R1A.B.solve)
+  , ("2017-Q-A", Y2017.Q.A.solve)
+  , ("2017-Q-B", Y2017.Q.B.solve)
+  , ("2017-Q-C", Y2017.Q.C.solve)
+  , ("2017-Q-D", Y2017.Q.D.solve)]
---
+++ src/gcj-lib/Y2017/Q/D.hs
@@ -0,0 +1,33 @@
+module Y2017.Q.D where
+
+import Protolude
+
+import qualified Data.Text as Text (concat)
+--import Data.List.Split
+import Data.String
+
+solve :: Text -> Text
+solve =  Text.concat . map write . zip [1..] .  map solve' . parse . drop 1 . lines . toS
+
+data P = P
+  deriving Show
+
+data S = S
+
+parse :: [String] -> [P]
+solve' :: P -> S
+write :: (Int, S) -> Text
+
+read :: (Read a) => a -> String -> a
+read d = maybe d fst . head . reads
+
+parse _ = []
+
+{-> parse . drop 1 . lines $ "2\n5\n3\n1 1 1\n2 1 0 2 0\n1 5 0\n1\n2\n1 1 0\n1 1 1\n"
+
+[P {n = 5, ms = [[(1,True)],[(0,False),(0,False)],[(0,False)]]},P {n = 1, ms = [[(0,False)],[(1,True)]]}]
+-}
+
+solve' P = S
+
+write (i, S) = toS $ "Case #" ++ show i ++ ":" ++ "\n"
