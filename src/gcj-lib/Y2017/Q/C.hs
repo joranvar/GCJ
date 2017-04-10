dbba71cca6226dd22a1b2e7fb23e8da45b32ed49
joranvar <bart.post@gmail.com>
2017-04-10 19:26:19 +0200

    Add writeVerification to Y2017.Q.C

--- src/gcj-lib/Y2017/Q/C.hs
+++ src/gcj-lib/Y2017/Q/C.hs
@@ -7,7 +7,7 @@ import qualified Data.Text as Text (concat)
 import Data.String
 
 solve :: Bool -> Text -> Text
-solve _ =  Text.concat . map write . zip [1..] .  map solve' . parse . drop 1 . lines . toS
+solve blindly =  Text.concat . map write . zip [1..] .  map (\p -> let s = solve' p in (s, if blindly then (True, "") else verify p s)) . parse . drop 1 . lines . toS
 
 data P = P { n::Integer, k::Integer}
   deriving Show
@@ -16,7 +16,8 @@ data S = S Integer Integer
 
 parse :: [String] -> [P]
 solve' :: P -> S
-write :: (Int, S) -> Text
+write :: (Int, (S, (Bool, String))) -> Text
+verify :: P -> S -> (Bool, String)
 
 read :: (Read a) => a -> String -> a
 read d = maybe d fst . head . reads
@@ -31,6 +32,19 @@ parse _ = []
 [P {n = 4, k = 2},P {n = 5, k = 2},P {n = 6, k = 2},P {n = 1000, k = 1000},P {n = 1000, k = 1}]
 -}
 
-solve' P{..} = S 0 0
+solve' P{..} = uncurry S $ go' k []
+  where go' :: Integer -> [(Integer, Integer)] -> (Integer, Integer)
+        go' k _ = (1, 2)
+  -- let generation = (floor ((2 :: Double) `logBase` (fromIntegral k :: Double)) :: Int)
+  --     rank = fromIntegral $ k - (2 ^ generation) :: Int
+  --     maxLR = foldl (\s b -> if b then s `div` 2 else (s + 1) `div` 2) n $  map (\i -> testBit rank i) $ reverse [0..generation]
+  --     minLR = foldl (\s b -> if b then s `div` 2 else (s - 1) `div` 2) n $  map (\i -> testBit rank i) $ reverse [0..generation]
+  -- in S maxLR minLR
 
-write (i, S minLR maxLR) = toS $ "Case #" ++ show i ++ ": " ++ show minLR ++ " " ++ show maxLR ++ "\n"
+write (i, (S minLR maxLR, v)) = toS $ writeVerification v ++ "Case #" ++ show i ++ ": " ++ show minLR ++ " " ++ show maxLR ++ "\n"
+
+writeVerification :: (Bool, String) -> String
+writeVerification (True, _) = ""
+writeVerification (False, err) = "!(" ++ err ++ ")"
+
+verify P{..} _ = (True, "")
