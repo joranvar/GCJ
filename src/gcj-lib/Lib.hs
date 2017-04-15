582d018169965bbadf045e660e7db7a447fe8372
joranvar <bart.post@gmail.com>
2017-04-15 02:50:44 +0200

    Prepare for round 1A

--- src/gcj-lib/Lib.hs
+++ src/gcj-lib/Lib.hs
@@ -4,24 +4,14 @@ import Protolude
 
 import qualified Data.Map as Map
 
-import qualified Y2008.Q.A (solve)
-import qualified Y2008.Q.B (solve)
-import qualified Y2008.Q.C (solve)
-import qualified Y2008.R1A.A (solve)
-import qualified Y2008.R1A.B (solve)
-import qualified Y2017.Q.A (solve)
-import qualified Y2017.Q.B (solve)
-import qualified Y2017.Q.C (solve)
-import qualified Y2017.Q.D (solve)
+import qualified Y2017.R1A.A (solve)
+import qualified Y2017.R1A.B (solve)
+import qualified Y2017.R1A.C (solve)
+import qualified Y2017.R1A.D (solve)
 
 runner :: Text -> Maybe (Bool -> Text -> Text)
 runner = flip Map.lookup $ Map.fromList
-  [ ("2008-Q-A", Y2008.Q.A.solve)
-  , ("2008-Q-B", Y2008.Q.B.solve)
-  , ("2008-Q-C", Y2008.Q.C.solve)
-  , ("2008-R1A-A", Y2008.R1A.A.solve)
-  , ("2008-R1A-B", Y2008.R1A.B.solve)
-  , ("2017-Q-A", Y2017.Q.A.solve)
-  , ("2017-Q-B", Y2017.Q.B.solve)
-  , ("2017-Q-C", Y2017.Q.C.solve)
-  , ("2017-Q-D", Y2017.Q.D.solve)]
+  [ ("2017-R1A-A", Y2017.R1A.A.solve)
+  , ("2017-R1A-B", Y2017.R1A.B.solve)
+  , ("2017-R1A-C", Y2017.R1A.C.solve)
+  , ("2017-R1A-D", Y2017.R1A.D.solve) ]
---
+++ src/gcj-lib/Y2017/R1A/A.hs
@@ -0,0 +1,39 @@
+module Y2017.R1A.A where
+
+import Protolude
+
+import Data.String
+
+import GCJ
+
+parse :: [String] -> [P]
+solve' :: P -> S
+write :: S -> Text
+verify :: P -> S -> Verification
+solve :: Bool -> Text -> Text
+solve = GCJ.solve (Parse parse) (Solve solve') (Write write) (Verify verify)
+
+example :: String
+example = ""
+
+data P = P deriving Show
+data S = S deriving Show
+
+parse (_:rest) = P : parse rest
+parse _ = []
+
+{-> parse . drop 1 . lines $ example
+
+[]
+-}
+
+solve' P = S
+
+{-> map solve' $ parse . drop 1 . lines $ example
+
+[]
+-}
+
+write S = toS $ " " ++ "\n"
+
+verify P S = Nothing
