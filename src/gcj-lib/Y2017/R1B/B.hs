2dd7f383d7743bb6c76b701796653a3bd56863a5
joranvar <bart.post@gmail.com>
2017-04-22 19:41:30 +0200

    Create an (apparently) slow solver

--- src/gcj-lib/Y2017/R1B/B.hs
+++ src/gcj-lib/Y2017/R1B/B.hs
@@ -18,7 +18,7 @@ example = "4\n6 2 0 2 0 2 0\n3 1 0 2 0 0 0\n6 2 0 1 1 2 0\n4 0 0 2 0 0 2\n"
 
 data P = P { r,o,y,g,b,v::Int
            , n::Int  } deriving Show
-data S = Impossible | S [Char] deriving Show
+data S = Impossible | S [Char] deriving (Show, Eq)
 
 parse (unicorns:rest) =
   let (n:r:o:y:g:b:v:_) = map (read 0) $ words unicorns
@@ -30,21 +30,39 @@ parse _ = []
 [P {r = 2, o = 0, y = 2, g = 0, b = 2, v = 0, n = 6},P {r = 1, o = 0, y = 2, g = 0, b = 0, v = 0, n = 3},P {r = 2, o = 0, y = 1, g = 1, b = 2, v = 0, n = 6},P {r = 0, o = 0, y = 2, g = 0, b = 0, v = 2, n = 4}]
 -}
 
-solve' P{..} =
-  Impossible
+solve' p' =
+  let
+    firstBuild :: [([Char], P)] -> S
+    firstBuild = fromMaybe Impossible . head . dropWhile (==Impossible) . map (uncurry build)
+    makeBuild :: [Char] -> P -> Char -> ([Char], P)
+    makeBuild sol p@P{..} 'R' = (('R':sol), p{r=r-1})
+    makeBuild sol p@P{..} 'O' = (('O':sol), p{o=o-1})
+    makeBuild sol p@P{..} 'Y' = (('Y':sol), p{y=y-1})
+    makeBuild sol p@P{..} 'G' = (('G':sol), p{g=g-1})
+    makeBuild sol p@P{..} 'B' = (('B':sol), p{b=b-1})
+    makeBuild sol p@P{..} 'V' = (('V':sol), p{v=v-1})
+    makeBuild _ _ _ = undefined
 
-{-> map (oneEach . map snd . ingredients) $ parse . drop 1 . lines $ example
-
-[[[900,660]],[[1500,809]],[[450,1100],[449,1100],[450,1101],[449,1101]],[[300,500]],[[11],[13],[17],[11],[16],[14],[12],[18]],[[1260,800,1700],[1500,800,1700],[700,800,1700],[1260,1440,1700],[1500,1440,1700],[700,1440,1700],[1260,1600,1700],[1500,1600,1700],[700,1600,1700],[1260,800,1620],[1500,800,1620],[700,800,1620],[1260,1440,1620],[1500,1440,1620],[700,1440,1620],[1260,1600,1620],[1500,1600,1620],[700,1600,1620],[1260,800,900],[1500,800,900],[700,800,900],[1260,1440,900],[1500,1440,900],[700,1440,900],[1260,1600,900],[1500,1600,900],[700,1600,900]]]
--}
+    build :: [Char] -> P -> S
+    build sol P{..} | (length sol) == n && (head sol /= (head $ reverse sol)) = S sol
+    build sol P{..} | (length sol) == n = Impossible
+    build sol@('R':_) p@P{..} = firstBuild $ map snd $ filter ((>0) . fst) [(y, makeBuild sol p 'Y'), (g, makeBuild sol p 'G'), (b, makeBuild sol p 'B')]
+    build sol@('O':_) p@P{..} = firstBuild $ map snd $ filter ((>0) . fst) [(b, makeBuild sol p 'B')]
+    build sol@('Y':_) p@P{..} = firstBuild $ map snd $ filter ((>0) . fst) [(r, makeBuild sol p 'R'), (v, makeBuild sol p 'V'), (b, makeBuild sol p 'B')]
+    build sol@('G':_) p@P{..} = firstBuild $ map snd $ filter ((>0) . fst) [(r, makeBuild sol p 'R')]
+    build sol@('B':_) p@P{..} = firstBuild $ map snd $ filter ((>0) . fst) [(r, makeBuild sol p 'R'), (o, makeBuild sol p 'O'), (y, makeBuild sol p 'Y')]
+    build sol@('V':_) p@P{..} = firstBuild $ map snd $ filter ((>0) . fst) [(y, makeBuild sol p 'Y')]
+    build sol p@P{..} = firstBuild $ map snd $ filter ((>0) . fst) [(r, makeBuild sol p 'R'), (o, makeBuild sol p 'O'), (y, makeBuild sol p 'Y')
+                                                                   ,(g, makeBuild sol p 'G'), (b, makeBuild sol p 'B'), (v, makeBuild sol p 'V')]
+ in build "" p'
 
 {-> map solve' $ parse . drop 1 . lines $ example
 
-[S 1,S 0,S 1,S 0,S 3,S 9]
+[S "BYBRYR",Impossible,S "BYBRGR",S "VYVY"]
 -}
 
 write Impossible = " IMPOSSIBLE\n"
-write (S sol) = toS $ " " ++ show sol ++ "\n"
+write (S sol) = toS $ " " ++ sol ++ "\n"
 
 verify P{..} (S sol) = if length sol /= n then Just "Length mismatch!" else Nothing
 verify P{..} Impossible = Nothing
